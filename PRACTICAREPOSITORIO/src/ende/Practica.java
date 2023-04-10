3d5e8fbcc2866e8902fe796008b7cbac29632747
lucia <lucia@LAPTOP-Q47V7EFK>
2023-04-10 12:06:24 +0200

    Mensaje a Jose

--- PRACTICAREPOSITORIO/src/ende/Practica.java
+++ PRACTICAREPOSITORIO/src/ende/Practica.java
@@ -3,7 +3,7 @@ package ende;
 public class Practica {
 
 	public static void main(String[] args) {
-		System.out.println("Me cago en tu ****** m****");
+		System.out.println("Jose es muy g*");
 
 	}
 
